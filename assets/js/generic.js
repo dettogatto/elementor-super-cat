cba285cd36a9a999b065387d6ddef48f38850756
gatto <nicola.cavallazzi@gmail.com>
2019-11-11 12:48:01 +0100

    Fix

--- assets/js/generic.js
+++ assets/js/generic.js
@@ -1,20 +1,20 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+// document.addEventListener("DOMContentLoaded", function(event) {
 
-  var $jq = jQuery.noConflict();
+  // var $jq = jQuery.noConflict();
+  //
+  // $jq("form").submit(function(){
+  //   $jq(this).find('input, textarea, select').each(function(){
+  //     /* SET THE COOKIE */
+  //     var name = $jq(this).attr("name");
+  //     var value = $jq(this).val();
+  //     var expires = "";
+  //     var matches = name.match(/form\_fields\[(.*?)\]/);
+  //     if(matches){ name = matches[1]; }
+  //     name = "supercat_form_" + name;
+  //     // document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
+  //     localStorage.setItem(name, value);
+  //   });
+  // });
 
-  $jq("form").submit(function(){
-    $jq(this).find('input, textarea, select').each(function(){
-      /* SET THE COOKIE */
-      var name = $jq(this).attr("name");
-      var value = $jq(this).val();
-      var expires = "";
-      var matches = name.match(/form\_fields\[(.*?)\]/);
-      if(matches){ name = matches[1]; }
-      name = "supercat_form_" + name;
-      // document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
-      localStorage.setItem(name, value);
-    });
-  });
 
-
-});
+// });
